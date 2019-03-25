bf294633be5e769883286de45d376b437faaa465
Kenn <kenn+git@code-crew.org>
2019-03-25 11:45:26 -0500

    update

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var ToDoCollection = require('../models/ToDoCollection');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: "Kenn's Page" });
 });
 
+router.get('/results', (req,res)=>{
 
+  // console.log(req.query.idLabel);
+  // userId: Number,
+  //     id: Number,
+  //     title: String,
+  //     completed: {type:Boolean, default: false},
+
+  ToDoCollection.create(
+      {
+        userId: req.query.idLabel,
+        id: req.query.todoid,
+        title: req.query.todotitle,
+        completed: req.query.todocomplete,
+      }, (errors)=>{
+        if (errors) res.send(errors);
+        else res.send("Added!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+      }
+  );
+
+});
 
 module.exports = router;
